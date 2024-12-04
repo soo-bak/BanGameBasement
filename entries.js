6f46fb9199e4ab5db8428679a3b63533fa7b2656
soo-bak <znyous@gmail.com>
2024-12-04 17:19:41 +0900

    :rocket: Init

---
+++ entries.js
@@ -0,0 +1,29 @@
+/**
+ * Every entry must include a title and filename, the rest are optional.
+ * For simplicity in merges, please add to the bottom of the object.
+ *
+ * interface
+ * {
+ *     title: string // display name
+ *     filename: string // the name of your HTML file
+ *     description?: string // description that will be listed with your entry
+ *     author?: string // your name/tag that will be listed
+ *     github?: string // username on github that will display a link to your profile
+ *     compatibleBrowsers?: array // browsers that this page is compatible with
+ * }
+ */
+
+const entries = [
+	{
+		title: "LCK 시뮬레이터",
+		filename: "LCK-simulator.html",
+		description: "LCK 시뮬레이터 게임",
+		author: "유원",
+	},
+	{
+    title: "스시 마스터",
+		filename: "SUSHI MASTER.html",
+    description: "마스터-초밥왕에 도전하세요! (약간의 운과 약간의 전략, 약간의 컨트롤이 필요합니다.)",
+		author: "필근",
+	},
+];
